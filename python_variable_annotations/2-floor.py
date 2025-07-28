a584d87591b390e9ccd5cddda282cde9feed79df
Pulon Julien <pulonjulien@gmail.com>
2025-07-28 12:09:34 +0200

    3-to_str.py 2-floor pycode style

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -2,6 +2,7 @@
 """type-annotated function floor"""
 import math
 
+
 def floor(n: float) -> int:
     """type-annotated function floor which takes a float n as argument and returns
     the floor of the float."""
