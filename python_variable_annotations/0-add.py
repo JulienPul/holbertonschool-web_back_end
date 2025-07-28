28483499e2ec8a42cc46627d07c7789aa6220cbe
Pulon Julien <pulonjulien@gmail.com>
2025-07-28 10:38:21 +0200

    pycodestyle

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -2,10 +2,15 @@
 """This module provides a function that returns the sum of two float numbers."""
 
 def add(a: float, b: float) -> float:
-    """Add two float numbers and return the result as a float.
+    """
+    Add two float numbers and return the result as a float.
+
+    Args:
         a (float): The first float number to add.
         b (float): The second float number to add.
 
     Returns:
-        float: The result of the addition."""
+        float: The result of the addition.
+    """
+
     return a + b
