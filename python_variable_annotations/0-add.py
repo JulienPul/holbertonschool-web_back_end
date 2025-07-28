a6e94416c794120684660702734e2dda1a890a85
Pulon Julien <pulonjulien@gmail.com>
2025-07-28 10:23:09 +0200

    pycodestyle

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-#function return the sum as a float
+"""function return the sum as a float"""
 
-def add(a: float, b:float) -> float:
-    # type annotated function that takes and return floats
+def add(a: float, b: float) -> float:
+    """type annotated function that takes and return floats"""
     result = a + b
     return result
