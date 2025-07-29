009b14cdbbcabfe3087c471d3c4480a1bd71c98b
Pulon Julien <pulonjulien@gmail.com>
2025-07-29 11:24:43 +0200

    gather instead of sleep

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 """Let's execute multiple coroutines at the same time with async"""
-wait_random = __import__('0-basic_async_syntax').wait_random
-import _asyncio
+import asyncio
 from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> (List[float]):
+async def wait_n(n: int, max_delay: int) -> List[float]:
     coroutine = []
-    for i in range (0, n):
+    for _ in range (n):
         coroutine.append(wait_random(max_delay)) 
-    delays = await _asyncio.sleep(*coroutine)
+    delays = await asyncio.gather(*coroutine)
     return delays
-    
