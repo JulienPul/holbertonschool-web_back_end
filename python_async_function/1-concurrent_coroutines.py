7026fd95b516c917ce448570df7c3b2a037f3e15
Pulon Julien <pulonjulien@gmail.com>
2025-07-29 17:51:44 +0200

    add comment to 12 an3 functions

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -6,6 +6,9 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
+    """async routine wait_n that takes 2 int arguments n and max_delay.
+    spawn wait_random n times with the specified max_delay.
+    wait_n return the list of all the delays (float values)"""
     coroutine = [wait_random(max_delay) for _ in range(n)]
     delays = await asyncio.gather(*coroutine)
     return delays
