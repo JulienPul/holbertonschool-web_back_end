160d79d2dc649679e1e6d9a0c143be016f701cd4
Pulon Julien <pulonjulien@gmail.com>
2025-07-29 09:02:00 +0200

    6-sum_mixed_list.py add typing library and list union

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """type-annotated function sum_mixed_list"""
+from typing import List, Union
 
-def sum_mixed_list(mxd_lst: int, float) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """ takes a list mxd_lst of integers and floats and returns
     their sum as a float."""
     return sum(mxd_lst)
