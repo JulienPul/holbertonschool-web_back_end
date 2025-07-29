f46361502db7446e644b41f45ea9b5afeb05d24b
Pulon Julien <pulonjulien@gmail.com>
2025-07-29 09:24:42 +0200

    7-to_kv.py complete return with Tple from library

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -2,6 +2,7 @@
 """Complex types - string and int/float to tuple"""
 from typing import Union, Tuple
 
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """to_kv that takes a string k and an int OR float v as arguments and returns a tuple.
     The first element of the tuple is the string k.
