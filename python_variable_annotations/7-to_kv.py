eefbe675f1d53219d629c7d2957c12c71f879f56
Pulon Julien <pulonjulien@gmail.com>
2025-07-29 09:19:59 +0200

    7-to_kv.py complete return with str and float

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -2,7 +2,7 @@
 """Complex types - string and int/float to tuple"""
 from typing import List, Union
 
-def to_kv(k: str, v: List[Union[int, float]]) -> tuple:
+def to_kv(k: str, v: List[Union[int, float]]) -> tuple[str, float]:
     """to_kv that takes a string k and an int OR float v as arguments and returns a tuple.
     The first element of the tuple is the string k.
     The second element is the square of the int/float v
