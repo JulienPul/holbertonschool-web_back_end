c385c7426e8114244d4226be6761e953cde1a261
Pulon Julien <pulonjulien@gmail.com>
2025-07-29 09:48:14 +0200

    8-make_multiplier.py fix pycode missing whitespace

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -4,7 +4,7 @@
 from typing import Callable
 
 
-def make_multiplier(multiplier: float) -> Callable[[float],float]:
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """type-annotated function make_multiplier
     that takes a float multiplier as argument and
     returns a function that multiplies a float by multiplier."""
