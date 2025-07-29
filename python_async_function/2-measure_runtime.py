9bdbd5c7e768f3e96c058a56ebfa5381c239bc53
Pulon Julien <pulonjulien@gmail.com>
2025-07-29 15:31:40 +0200

    pycodestyle remove trailing space

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -6,8 +6,8 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
-    start_time = time.time()    
-    asyncio.run(wait_n(n, max_delay))    
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
     end_time = time.time()
     elapsed_time = end_time - start_time
-    return elapsed_time/ n
+    return elapsed_time / n
