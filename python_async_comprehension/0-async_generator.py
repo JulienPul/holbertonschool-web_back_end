540a0fcb544f653243b9a0bdfa200704c11a4eea
Pulon Julien <pulonjulien@gmail.com>
2025-07-30 10:34:30 +0200

    add Asyncgenerator annotation

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""Async Generator"""
+"""Async Generator coroutine 
+called async_generator that takes no arguments."""
 import random
 import asyncio
 from typing import AsyncGenerator
