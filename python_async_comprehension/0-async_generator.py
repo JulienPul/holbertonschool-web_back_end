928c2ca60843b819911e62ea6d7786fc6f4acd1b
Pulon Julien <pulonjulien@gmail.com>
2025-07-30 10:27:03 +0200

    fix yield indentation

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,7 +5,9 @@ import asyncio
 
 
 async def async_generator():
+    """coroutine will loop 10 times,
+    each time asynchronously wait 1 second, then yield a random number
+    between 0 and 10. Use the random module."""
     for _ in range(10):
         await asyncio.sleep(1)
-    return yield.random(0, 10)
-
+        yield random.uniform(0, 10)
