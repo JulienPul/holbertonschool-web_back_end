d99fb0ab756c92105eecaa9137c73463d3b95bfb
Pulon Julien <pulonjulien@gmail.com>
2025-07-30 10:52:02 +0200

    1-async_comprehension.py

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,10 @@
+#!/usr/bin//env python3
+"""Async Comprehensions"""
+async_generator = __import__('0-async_generator').async_generator
+from typing import AsyncGenerator
+import asyncio
+
+
+async def asyn_comprehension() -> AsyncGenerator[float, None]:
+    random_n = await asyncio.gather(async_generator)
+    return random_n
