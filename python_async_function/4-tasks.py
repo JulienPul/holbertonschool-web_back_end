c4801800e638f2dc76c85572abfb2e57f5595bac
Pulon Julien <pulonjulien@gmail.com>
2025-07-30 10:02:21 +0200

    4-tasks.py sorted delays

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -6,9 +6,7 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """async routine wait_n that takes 2 int arguments n and max_delay.
-    spawn wait_random n times with the specified max_delay.
-    wait_n return the list of all the delays (float values)"""
+    """from wait_n and alter it into a new function task_wait_n"""
     coroutine = [task_wait_random(max_delay) for _ in range(n)]
     delays = await asyncio.gather(*coroutine)
-    return(delays)
+    return sorted(delays)
