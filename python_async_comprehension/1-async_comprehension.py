ac01062e746c44243f21197973d3b8f9c2711c8a
Pulon Julien <pulonjulien@gmail.com>
2025-07-31 14:32:01 +0200

    1-async_comprehension.py

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,10 +1,10 @@
-#!/usr/bin//env python3
+#!/usr/bin/env python3
 """Async Comprehensions"""
 async_generator = __import__('0-async_generator').async_generator
-from typing import AsyncGenerator
 import asyncio
+from typing import List
 
 
-async def asyn_comprehension() -> AsyncGenerator[float, None]:
+async def async_comprehension() -> List[float]:
     random_n = [v async for v in async_generator()]
     return random_n
