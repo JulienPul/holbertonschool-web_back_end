e5a365824c7a9178744461f93ac27c7aa4ac2742
Pulon Julien <pulonjulien@gmail.com>
2025-08-12 09:34:56 +0200

    1-simple_pagination.py

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""index_range + get_page (1-simple_pagination.py)"""
+import csv
+from typing import Tuple, List
+import math
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """takes two integer arguments page and page_size
+    start index and an end index
+    corresponding to the range of indexes to return in
+    a list for those particular pagination parameters."""
+
+    start = (page - 1) * page_size
+    end = page * page_size
+    return (start, end)
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+            pass
