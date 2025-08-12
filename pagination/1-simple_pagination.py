7ed0f26f928de66a42a9201c19f0ec13b713a8df
Pulon Julien <pulonjulien@gmail.com>
2025-08-12 09:37:12 +0200

    1-simple_pagination.py

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -35,4 +35,12 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-            pass
+            assert isinstance(page, int) and page > 0
+            assert isinstance(page_size, int) and page_size> 0
+    
+            start, end = index_range(page, page_size)
+            dataset = self.dataset
+
+            if start > dataset:
+                return []
+            return dataset[start, end]
