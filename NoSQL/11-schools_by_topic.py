473e98d1aea28d2b727e01bb9747f729e1f0d3da
Pulon Julien <pulonjulien@gmail.com>
2025-08-14 15:00:17 +0200

    11 update

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Module  function that returns the list of school having a specific topic"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """function that returns the list of school having a specific topic:
+    Prototype: def schools_by_topic(mongo_collection, topic):
+    mongo_collection will be the pymongo collection object
+    topic (string) will be topic searched"""
+
+    return mongo_collection.find({"topics": topic})
