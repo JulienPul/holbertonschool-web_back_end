fa6e04980fd3d95a6683acd22d38db1d1e898aad
Pulon Julien <pulonjulien@gmail.com>
2025-08-21 12:25:32 +0200

    4-pricing.js

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,40 @@
+// Pricing
+import Currency from "./3-currency.js";
+export default class Pricing {
+  constructor (amount, currency){
+    this._amount = amount;
+    this._currency = Currency;
+  }
+  get ammount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof value !== 'number') {
+        throw new TypeError('Amount must be a number');
+    }
+    this._amount = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (typeof value !== 'string') {
+        throw new TypeError('Currency must be a string');
+    }
+    this._currency = value
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
+      throw new TypeError('Amount an conversionRate must be numbers');
+    }
+    return amount * conversionRate;
+  }
+}
