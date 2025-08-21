f42acfe1c95d218476cdeed47f162eb772f42e20
Pulon Julien <pulonjulien@gmail.com>
2025-08-21 10:12:27 +0200

    2-hbtn_course.js

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,44 @@
+// A Course, Getters, and Setters
+export default Class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(value) {
+    if (typeof value !== 'number') {
+      TypeError('Length must be a number')
+    }
+    this._length = value;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(value) {
+    if (!Array.isArray(value)) {
+      TypeError('Students must be an array');
+    }
+    if (!value.every((s) => typeof s == 'string')) {
+      TypeError('Student must be a string')
+    }
+    this._students = value;
+  }
+}
