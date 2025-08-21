188959c9acd208d2413076721424335ca90d082c
Pulon Julien <pulonjulien@gmail.com>
2025-08-21 10:14:32 +0200

    2-hbtn_course.js fix class in lowercase

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,5 +1,5 @@
 // A Course, Getters, and Setters
-export default Class HolbertonCourse {
+export default class HolbertonCourse {
   constructor(name, length, students) {
     this._name = name;
     this._length = length;
