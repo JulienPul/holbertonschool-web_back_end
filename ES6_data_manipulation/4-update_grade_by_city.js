4d5b16f34a24966f1c4a6ac2cad2653003326178
Pulon Julien <pulonjulien@gmail.com>
2025-08-25 17:35:53 +0200

    4-update_grade_by_city.js

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,13 @@
+// Combine
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
+      if (gradeObj) {
+        return { ...student, grade: gradeObj.grade };
+      } else {
+        return { ...student, grade: 'N/A' };
+      }
+    });
+}
