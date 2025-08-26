9934c13022ade61d60902e3f6f086b6d9e45afe2
Pulon Julien <pulonjulien@gmail.com>
2025-08-26 13:08:53 +0200

    5-typed_arrays.js

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,11 +1,11 @@
 // Typed Arrays
 export default function createInt8TypedArray(length, position, value) {
-  let buffer = new ArrayBuffer(length);
-  let view = new DataView(buffer);
-  view.setInt8(position, value);
-    if (position <= 0 || position >= length) {
+    if (position < 0 || position >= length) {
         throw new Error("Position outside range");
     }
+    let buffer = new ArrayBuffer(length);
+    let view = new DataView(buffer);
+    view.setInt8(position, value);
 
     return view;
 }
