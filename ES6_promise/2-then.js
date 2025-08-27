00c5fad6fcafd45de5a233d54b0ad8af5e7c42a3
Pulon Julien <pulonjulien@gmail.com>
2025-08-27 15:51:02 +0200

    2-then.js

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,12 @@
+// promise finally
+export default function handleResponseFromAPI(promise) {
+   return new Promise((resolve, reject) => {
+    if (success) {
+      resolve ({ status: 200, body: 'Success'});
+    } else {
+      reject( new Error(''));
+    }
+  });
+promise
+  .finally(() => console.log("Got a response from the API"));
+}
