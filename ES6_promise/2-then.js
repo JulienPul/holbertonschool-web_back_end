2a95d6cf0248da03dd7086af070d620d6c486886
Pulon Julien <pulonjulien@gmail.com>
2025-08-27 15:57:02 +0200

    2-then.js then catch finally

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,12 +1,9 @@
 // promise finally
 export default function handleResponseFromAPI(promise) {
-   return new Promise((resolve, reject) => {
-    if (success) {
-      resolve ({ status: 200, body: 'Success'});
-    } else {
-      reject( new Error(''));
-    }
-  });
-promise
-  .finally(() => console.log("Got a response from the API"));
+   return promise
+   .then(() => ({ status: 200, body: 'Success'}))
+   .catch(() => new Error(''))
+   .finally(() => {
+         console.log("Got a response from the API");
+   });
 }
