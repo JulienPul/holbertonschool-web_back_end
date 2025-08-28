b3925d3a686c77d8143d384d0a40b953872af717
Pulon Julien <pulonjulien@gmail.com>
2025-08-28 10:05:07 +0200

    5-photo-reject.js fix backticks for reject

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,10 +1,10 @@
 // reject promises
 export default function uploadPhoto(filename) {
     const promise = new Promise((resolve, reject) => {
-      reject("${fileName} cannot be processed");
+      reject(`${filename} cannot be processed`);
     });
     promise
     .then(value => console.log("Success", value))
     .catch(error => console.log("Error:", error));
-    return promise
+    return promise;
 }
