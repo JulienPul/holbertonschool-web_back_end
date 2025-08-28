6493353c920c9034a7d2cfc56f3a11ad46ed5188
Pulon Julien <pulonjulien@gmail.com>
2025-08-28 14:43:31 +0200

    6-final-user.js add .js to imports

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,6 +1,6 @@
 // Handle multiple promises
-import signUpUser from './4-user-promise.js'
-import uploadPhoto from './5-photo-reject.js'
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
 export default function handleProfileSignup (firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
