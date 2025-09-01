a42feed52f2d0586e206602669a728b91bc04aca
Pulon Julien <pulonjulien@gmail.com>
2025-09-01 10:52:34 +0200

    add \n

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -5,6 +5,6 @@ process.stdin.on('data', (data) => {
   const name = data.trim();
   process.stdout.write(`Your name is: ${name}\n`);
 });
-process.stdin.on('end', () => {
+process.on('exit'), () => {
   process.stdout.write('This important software is now closing\n');
-});
+};
