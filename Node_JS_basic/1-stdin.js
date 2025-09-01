38200d1c258d7f353416e0ae02950f12d735ce6c
Pulon Julien <pulonjulien@gmail.com>
2025-09-01 10:22:47 +0200

    1-stdin.js

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,9 @@
+//  Using Process stdin
+process.stdout.write(`Welcome to Holberton School, what is your name?`);
+process.stdin.on('data', (data) => {
+  const name = data.toString();
+  process.stdout.write(`Your name is: ${name}\n`);
+});
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
