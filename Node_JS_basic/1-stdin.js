a69052d2d4f9e92ea017be75b1f82df09d9fe7c6
Pulon Julien <pulonjulien@gmail.com>
2025-09-01 10:33:09 +0200

    add \n

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,5 +1,5 @@
 //  Using Process stdin
-process.stdout.write(`Welcome to Holberton School, what is your name?`);
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   process.stdout.write(`Your name is: ${name}\n`);
