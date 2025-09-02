4a68c0f1f0e3695d4d4d8af55d95c7272e1d1789
Pulon Julien <pulonjulien@gmail.com>
2025-09-02 10:24:14 +0200

    remove exit

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,13 +1,13 @@
 //  Using Process stdin
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
 process.stdin.setEncoding('utf8');
+
 process.stdin.on('data', (data) => {
   const name = data.trim();
   process.stdout.write(`Your name is: ${name}\n`);
 });
+
 process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
-});
-process.on('exit', () => {
   process.stdout.write('This important software is now closing\n');
 });
